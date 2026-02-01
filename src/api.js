445236525c912ef700c59fe108b82fc37a900b37
SakthiSivaramManikandan <sakthisivaram62@gmail.com>
2026-02-01 14:01:09 +0200

    added changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "517d8718";
-const API_URL = "http://www.omdbapi.com/"
+const API_URL = "https://www.omdbapi.com/"
 
 export const SearchMovie = async(searchTerm, type="") => {
     try {
